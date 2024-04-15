f7447de1e985976c48ffa9b55cc66706c797add1
Lev Kozlov <kozlov.l.a10@gmail.com>
2024-04-15 13:02:53 +0300

    feat: move math to module

---
+++ mujoco_sysid/modeling.py
@@ -0,0 +1,151 @@
+"""This module contains functions with easier access to dynamics quantities"""
+
+import mujoco
+import numpy as np
+
+
+def spacial_vel(model, data, body_idx: int, flg_local: int = 1):
+    rotlin_vel = np.zeros(6)
+
+    mujoco.mj_objectVelocity(model, data, 2, body_idx + 1, rotlin_vel, flg_local)
+
+    # by default it is rotational, then linear
+    linrot_vel = rotlin_vel.copy()
+    linrot_vel[:3], linrot_vel[3:] = rotlin_vel[3:], rotlin_vel[:3]
+
+    return linrot_vel
+
+
+def skew(v):
+    return np.array([[0, -v[2], v[1]], [v[2], 0, -v[0]], [-v[1], v[0], 0]])
+
+
+def spacial_acc(model, data, body_idx: int, flg_local: int = 1):
+    """Get spacial acceleration of chosen XBODY
+
+    We assume that `mujoco.mj_rnePostConstraint(mj_model, mj_data)`
+    is already called in prior to this function
+
+    Args:
+        model (_type_): _description_
+        data (_type_): _description_
+        body_idx (int): _description_
+        flg_local (int, optional): _description_. Defaults to 1.
+
+    Returns:
+        _type_: _description_
+    """
+    rotlin_acc = np.zeros(6)
+
+    mujoco.mj_objectAcceleration(model, data, 2, body_idx + 1, rotlin_acc, flg_local)
+
+    # by default it is rotational, then linear
+    linrot_acc = rotlin_acc.copy()
+    linrot_acc[:3], linrot_acc[3:] = rotlin_acc[3:], rotlin_acc[:3]
+
+    rotation = data.xmat[body_idx + 1].reshape(3, 3).T.copy()
+
+    vel = spacial_vel(model, data, body_idx, flg_local)
+
+    # compensate
+    linrot_acc[:3] += rotation @ model.opt.gravity - skew(vel[3:]) @ vel[:3]
+
+    return linrot_acc
+
+
+def Y_body(v_lin, v_ang, a_lin, a_ang):
+    # Derivation is here: https://colab.research.google.com/drive/1xFte2FT0nQ0ePs02BoOx4CmLLw5U-OUZ?usp=sharing
+    # this is identical to the pinnochio: pin.bodyRegressor(*[v_lin, v_ang], *[a_lin, a_ang])
+    v1, v2, v3 = v_lin
+    v4, v5, v6 = v_ang
+
+    a1, a2, a3 = a_lin
+    a4, a5, a6 = a_ang
+
+    # fmt: off
+    Y = np.array([
+        [a1 - v2*v6 + v3*v5, -v5**2 - v6**2, -a6 + v4*v5, a5 + v4*v6, 0, 0, 0, 0, 0, 0],
+        [a2 + v1*v6 - v3*v4, a6 + v4*v5, -v4**2 - v6**2, -a4 + v5*v6, 0, 0, 0, 0, 0, 0],
+        [a3 - v1*v5 + v2*v4, -a5 + v4*v6, a4 + v5*v6, -v4**2 - v5**2, 0, 0, 0, 0, 0, 0],
+        [0, 0, a3 - v1*v5 + v2*v4, -a2 - v1*v6 + v3*v4, a4, a5 - v4*v6, -v5*v6, a6 + v4*v5, v5**2 - v6**2, v5*v6],
+        [0, -a3 + v1*v5 - v2*v4, 0, a1 - v2*v6 + v3*v5, v4*v6, a4 + v5*v6, a5, -v4**2 + v6**2, a6 - v4*v5, -v4*v6],
+        [0, a2 + v1*v6 - v3*v4, -a1 + v2*v6 - v3*v5, 0, -v4*v5, v4**2 - v5**2, v4*v5, a4 - v5*v6, a5 + v4*v6, a6]
+    ])
+    # fmt: on
+
+    return Y
+
+
+def mj_bodyRegressor(mj_model, mj_data, body):
+    # TODO:
+    # Clarify more about transformations:
+    # https://mujoco.readthedocs.io/en/stable/programming/simulation.html#coordinate-frames-and-transformations
+    # https://royfeatherstone.org/teaching/index.html
+    # https://royfeatherstone.org/teaching/2008/slides.pdf
+
+    body_id = body + 1
+    velocity = np.zeros(6)
+    accel = np.zeros(6)
+    _cross = np.zeros(3)
+
+    mujoco.mj_objectVelocity(mj_model, mj_data, 2, body_id, velocity, 1)
+    mujoco.mj_rnePostConstraint(mj_model, mj_data)
+    mujoco.mj_objectAcceleration(mj_model, mj_data, 2, body_id, accel, 1)
+    # rotation = mj_data.xmat[body_id].reshape(3, 3).copy()
+
+    v, w = velocity[3:], velocity[:3]
+    dv, dw = accel[3:], accel[:3]  # dv - classical acceleration, already containt g
+
+    mujoco.mju_cross(_cross, w, v)
+
+    dv -= _cross
+
+    Y = Y_body(v, w, dv, dw)
+    return Y
+
+
+def jacobian(model, data, idx):
+    jac_lin = np.zeros((3, model.nv))
+    jac_rot = np.zeros((3, model.nv))
+
+    rotation = data.xmat[idx + 2].reshape(3, 3).T.copy()
+
+    mujoco.mj_jacPointAxis(
+        model,
+        data,
+        jac_lin,
+        None,
+        data.xanchor[idx],
+        data.xaxis[idx],
+        idx + 1,
+    )
+    mujoco.mj_jacBody(
+        model,
+        data,
+        None,
+        jac_rot,
+        idx + 2,
+    )
+
+    return np.vstack((rotation @ jac_lin, rotation @ jac_rot))
+
+
+def mj_jointRegressor(mj_model, mj_data):
+    # what the hell is going on with indeces?
+    # increase time in computation of multiplication between rotation
+    # Investigate spatial tranfromation mju_transformSpatial t
+
+    njoints = mj_model.njnt
+    body_regressors = np.zeros((6 * njoints, njoints * 10))
+    col_jac = np.zeros((6 * njoints, mj_model.nv))
+
+    for i in range(njoints):
+        # calculate cody regressors
+        body_regressors[6 * i : 6 * (i + 1), 10 * i : 10 * (i + 1)] = mj_bodyRegressor(
+            mj_model, mj_data, i + 1
+        )  # regressor_body(i+1)
+
+        col_jac[6 * i : 6 * i + 6, :] = jacobian(mj_model, mj_data, i)
+
+    joint_regressor = col_jac.T @ body_regressors
+    return joint_regressor
---
+++ tests/test_pin_match.py
@@ -0,0 +1,102 @@
+import pinocchio as pin
+import mujoco
+from robot_descriptions.z1_description import URDF_PATH
+from robot_descriptions.z1_mj_description import MJCF_PATH
+import numpy as np
+
+from mujoco_sysid import spacial_vel, spacial_acc, jacobian, mj_jointRegressor
+
+pinmodel = pin.buildModelFromUrdf(URDF_PATH)
+pindata = pinmodel.createData()
+
+mjmodel = mujoco.MjModel.from_xml_path(MJCF_PATH)
+mjdata = mujoco.MjData(mjmodel)
+
+
+def test_velocity():
+    q = np.random.randn(mjmodel.nq)
+    v = np.random.randn(mjmodel.nv)
+    dv = np.random.randn(mjmodel.nv)
+
+    mjdata.qpos[:] = q.copy()
+    mjdata.qvel[:] = v.copy()
+    mjdata.qacc[:] = dv.copy()
+
+    pin.computeAllTerms(pinmodel, pindata, q, v)
+    pin.forwardKinematics(pinmodel, pindata, q, v, dv)
+
+    mujoco.mj_inverse(mjmodel, mjdata)
+
+    for idx in range(mjmodel.nv):
+        pinvel = pin.getVelocity(pinmodel, pindata, idx, pin.LOCAL)
+        mjvel = spacial_vel(mjmodel, mjdata, idx)
+
+        np.testing.assert_allclose(pinvel, mjvel, atol=1e-6)
+
+
+def test_acceleration():
+    q = np.random.randn(mjmodel.nq)
+    v = np.random.randn(mjmodel.nv)
+    dv = np.random.randn(mjmodel.nv)
+
+    mjdata.qpos[:] = q.copy()
+    mjdata.qvel[:] = v.copy()
+    mjdata.qacc[:] = dv.copy()
+
+    pin.computeAllTerms(pinmodel, pindata, q, v)
+    pin.forwardKinematics(pinmodel, pindata, q, v, dv)
+
+    mujoco.mj_inverse(mjmodel, mjdata)
+    mujoco.mj_rnePostConstraint(mjmodel, mjdata)
+
+    for idx in range(mjmodel.nv):
+        pinacc = pin.getAcceleration(pinmodel, pindata, idx, pin.LOCAL)
+        mjacc = spacial_acc(mjmodel, mjdata, idx)
+
+        np.testing.assert_allclose(pinacc, mjacc, atol=1e-6)
+
+
+# TODO: in this test linear jacobian does not match with pinocchio
+# def test_jacobian():
+#     q = np.random.randn(mjmodel.nq)
+#     v = np.random.randn(mjmodel.nv)
+#     dv = np.random.randn(mjmodel.nv)
+
+#     mjdata.qpos[:] = q.copy()
+#     mjdata.qvel[:] = v.copy()
+#     mjdata.qacc[:] = dv.copy()
+
+#     pin.computeAllTerms(pinmodel, pindata, q, v)
+#     pin.forwardKinematics(pinmodel, pindata, q, v, dv)
+
+#     mujoco.mj_inverse(mjmodel, mjdata)
+#     mujoco.mj_rnePostConstraint(mjmodel, mjdata)
+
+#     for idx in range(mjmodel.nv):
+#         idx = 4
+#         jacobian_pin = pin.getJointJacobian(pinmodel, pindata, idx, pin.LOCAL)
+
+#         jacobian_mj = jacobian(mjmodel, mjdata, idx)
+
+#         np.testing.assert_allclose(jacobian_pin, jacobian_mj, atol=1e-6)
+
+
+def test_joint_regressor():
+    q = np.random.randn(mjmodel.nq)
+    v = np.random.randn(mjmodel.nv)
+    dv = np.random.randn(mjmodel.nv)
+
+    mjdata.qpos[:] = q.copy()
+    mjdata.qvel[:] = v.copy()
+    mjdata.qacc[:] = dv.copy()
+
+    pin.computeAllTerms(pinmodel, pindata, q, v)
+    pin.forwardKinematics(pinmodel, pindata, q, v, dv)
+
+    mujoco.mj_inverse(mjmodel, mjdata)
+    mujoco.mj_rnePostConstraint(mjmodel, mjdata)
+
+    pin_regressor = pin.computeJointTorqueRegressor(pinmodel, pindata, q, v, dv)
+    mj_regressor = mj_jointRegressor(mjmodel, mjdata)
+
+    np.testing.assert_allclose(pin_regressor, mj_regressor, atol=1e-6)
