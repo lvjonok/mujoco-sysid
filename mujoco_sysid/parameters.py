d3cd4362d8157808e286f663c0a41d8fd8fe88f6
Lev Kozlov <kozlov.l.a10@gmail.com>
2024-06-25 08:28:33 +0300

    feat: add set_dynamic_parameters

--- mujoco_sysid/parameters.py
+++ mujoco_sysid/parameters.py
@@ -32,3 +32,41 @@ def get_dynamic_parameters(mjmodel, body_id) -> npt.ArrayLike:
     mjinertia = R.T @ np.diag(diag_inertia) @ R + shift
 
     return np.concatenate([mass, mass * rc, mjinertia[np.triu_indices(3)]])
+
+
+def set_dynamic_parameters(mjmodel, body_id, theta: npt.ArrayLike) -> None:
+    """Set the dynamic parameters to a body
+
+    Args:
+        mjmodel (mujoco.MjModel): The mujoco model
+        body_id (int): The id of the body
+        theta (npt.ArrayLike): The dynamic parameters of the body
+    """
+
+    mass = theta[0]
+    rc = theta[1:4] / mass
+    inertia = theta[4:]
+    inertia_full = np.zeros((3, 3))
+    inertia_full[np.triu_indices(3)] = inertia
+
+    # shift the inertia
+    inertia_full -= -mass * skew(rc) @ skew(rc)
+
+    # eigen decomposition
+    eigval, eigvec = np.linalg.eig(inertia_full)
+    R = eigvec
+    diag_inertia = eigval
+
+    # check if singular, then abort
+    if np.any(np.isclose(diag_inertia, 0)):
+        raise ValueError("Cannot deduce inertia matrix because RIR^T is singular.")
+
+    # set the mass
+    mjmodel.body(body_id).mass = mass
+    mjmodel.body(body_id).ipos = rc
+
+    # set the orientation
+    mujoco.mju_mat2Quat(mjmodel.body(body_id).iquat, R.flatten())
+
+    # set the inertia
+    mjmodel.body(body_id).inertia = diag_inertia
