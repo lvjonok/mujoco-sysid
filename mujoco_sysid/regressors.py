180d8c7ae8b84059848c6fd023e5a524f047b167
Lev Kozlov <kozlov.l.a10@gmail.com>
2024-07-03 14:23:41 +0300

    fix: temporary comment

--- mujoco_sysid/regressors.py
+++ mujoco_sysid/regressors.py
@@ -146,26 +146,26 @@ def joint_torque_regressor(mj_model, mj_data) -> npt.ArrayLike:
         npt.ArrayLike: regressor for the whole model
     """
 
-    njoints = mj_model.njnt
-    body_regressors = np.zeros((6 * njoints, njoints * 10))
-    col_jac = np.zeros((6 * njoints, mj_model.nv))
-    jac_lin = np.zeros((3, mj_model.nv))
-    jac_rot = np.zeros((3, mj_model.nv))
+    # njoints = mj_model.njnt
+    # body_regressors = np.zeros((6 * njoints, njoints * 10))
+    # col_jac = np.zeros((6 * njoints, mj_model.nv))
+    # jac_lin = np.zeros((3, mj_model.nv))
+    # jac_rot = np.zeros((3, mj_model.nv))
 
-    for i in range(njoints):
-        # calculate cody regressors
-        body_regressors[6 * i : 6 * (i + 1), 10 * i : 10 * (i + 1)] = joint_body_regressor(
-            mj_model, mj_data, i + body_offset
-        )
+    # for i in range(njoints):
+    #     # calculate cody regressors
+    #     body_regressors[6 * i : 6 * (i + 1), 10 * i : 10 * (i + 1)] = joint_body_regressor(
+    #         mj_model, mj_data, i + body_offset
+    #     )
 
-        mujoco.mj_jacBody(mj_model, mj_data, jac_lin, jac_rot, i + body_offset)
+    #     mujoco.mj_jacBody(mj_model, mj_data, jac_lin, jac_rot, i + body_offset)
 
-        # Calculate jacobians
-        rotation = mj_data.xmat[i + body_offset].reshape(3, 3).copy()
-        col_jac[6 * i : 6 * i + 3, :] = rotation.T @ jac_lin.copy()
-        col_jac[6 * i + 3 : 6 * i + 6, :] = rotation.T @ jac_rot.copy()
+    #     # Calculate jacobians
+    #     rotation = mj_data.xmat[i + body_offset].reshape(3, 3).copy()
+    #     col_jac[6 * i : 6 * i + 3, :] = rotation.T @ jac_lin.copy()
+    #     col_jac[6 * i + 3 : 6 * i + 6, :] = rotation.T @ jac_rot.copy()
 
-    return col_jac.T @ body_regressors
+    # return col_jac.T @ body_regressors
 
 
 def body_energyRegressor(
