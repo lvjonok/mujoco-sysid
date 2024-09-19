cc768d5539fd50bc89cc0e8feae2f46b6db1b845
Simeon <simkaned@gmail.com>
2024-09-19 11:46:18 +0300

    added new data and simple models

--- mujoco_sysid/mjx/residuals.py
+++ mujoco_sysid/mjx/residuals.py
@@ -1,3 +1,3 @@
 # TODO:
 # Implement a different residuals here
-# i.e: Joint torque residual, simulation residual, energy residual 
+# i.e: Joint torque residual, simulation residual, energy residual
